25d85ecf23d50474350422c5856f09e23394a1c8
Mayur Upadhayay <mayur123a@gmail.com>
2022-08-14 11:46:29 +0530

    feat: Adds Brand and Category entity

---
+++ ecom-api/app/Models/Category.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Category extends Model
+{
+    use HasFactory, SoftDeletes;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'category_name',
+        'category_desc'
+    ];
+    
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'category_name' => 'string',
+        'category_desc' => 'string'
+    ];
+}
