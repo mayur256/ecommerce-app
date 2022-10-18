6fad95d5dbb88e777e5bcc0fbf34e6be6c09ef7f
Mayur Upadhayay <mayur123a@gmail.com>
2022-10-18 23:19:28 +0530

    feat: Creates a simple model for order entity

---
+++ ecom-api/database/migrations/2022_10_18_172457_create_order_product_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_product', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->integer('order_id')->unsigned()->index();
+            $table->integer('product_id')->unsigned()->index();
+            $table->integer('quantity');
+            $table->integer('unit_price');
+            $table->timestamps(); // created_at, deleted_at
+            $table->softDeletes(); // deleted_at
+
+            // Foreign keys
+            $table->foreign('order_id')->references('id')->on('orders');
+            $table->foreign('product_id')->references('id')->on('products');
+
+            // Composite Primary key
+            $table->primary(['order_id', 'product_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_product');
+    }
+}
